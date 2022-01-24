40a0950cdb3ec7a55d7f10674401325075b54d28
Don Truong <dong23102003@gmail.com>
2022-01-23 16:03:03 -0800

    Save snapshot to local storage

--- public/js/support/snapshot.js
+++ public/js/support/snapshot.js
@@ -5,26 +5,33 @@
 import { validateParams, display, displayError, displayOk } from "./utility.js";
 
 var storage = undefined;
+
+function writeToFile(d) {
+	const data = JSON.stringify(d);
+	localStorage.setItem('snapshot', data);
+}
+
 export function save(inputArr , d) {
 	if (!validateParams(inputArr, 1, false)) { return; }
-
 	if (storage != undefined) {
 		display("WARNING: Overwrote a previous state", "yellow");
 	}
 
 	storage = Object.assign({}, d.dict);
+	writeToFile(d);
 	displayOk("Saved!");
 }
 
 export function restore(inputArr, d) {
 	if (!validateParams(inputArr, 1, false)) { return; }
-
-	if (storage == undefined) {
+	const data = JSON.parse(localStorage.getItem('snapshot'));
+	if (data == null) {
 		displayError("ERROR: Haven't saved yet");
 		return;
 	}
 
-	d.dict = Object.assign({}, storage);
+	d.dict = Object.assign({}, data.dict);
 	d.timeout = {};
+
 	displayOk("Restored!");
 }
